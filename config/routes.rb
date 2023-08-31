f0e2257510179b247f6e1d7ad083e64111d677d0
kohauke.takeda <kohsuke.takeda@gmail.com>
2023-08-31 00:30:56 +0000

    2-8 webpack.mix.jsを修正　エラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -9,8 +10,8 @@ Rails.application.routes.draw do
   get '/', to: 'posts#index', as: "index_post"
   #課題2-5 3【応用】
   #new Action   edit Action
-  get 'topics/create', to: 'topics#new', as: "new_topics"
+  get 'topics/new', to: 'topics#new', as: "new_topics"
   
-  get 'topics/update', to: 'topics#edit', as: "edit_topics"
+  get 'topics/edit', to: 'topics#edit', as: "edit_topics"
   
 end
