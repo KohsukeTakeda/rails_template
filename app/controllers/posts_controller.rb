cde1eea6f4fe798a36dc5c4fc40cd565d1e58f82
kohauke.takeda <kohsuke.takeda@gmail.com>
2023-09-14 08:56:43 +0000

    2-13終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     render :index
   end
 
-  # ここから
+  
   def new
     @post = Post.new
     render :new
@@ -25,15 +25,41 @@ class PostsController < ApplicationController
     end
 
     if @post.save
-      redirect_to index_post_path, notice: '登録しました'
+      redirect_to index_post_path, notice: '投稿しました'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+
+  # ここから追加
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if params[:post][:image]
+      @post.image.attach(params[:post][:image])
+    end
+    if @post.update(post_params)
+      redirect_to index_post_path, notice: '更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  # ここまで
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to index_post_path, notice: '削除しました'
+  end
+  
   private
   def post_params
     params.require(:post).permit(:title, :body, :image)
   end
-  # ここまで
+  
 end
